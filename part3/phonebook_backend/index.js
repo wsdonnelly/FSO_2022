105954766546fae1c8ae3efaf1d916edf765f43d
wsdonnelly <wsdonnelly@gmail.com>
2022-08-26 12:36:35 +0300

    removed comment from phonebook_backend index.js

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,7 +3,6 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
-//const {request, response} = require('express')
 const app = express()
 
 morgan.token('body', (req) => JSON.stringify(req.body))
